15284c0028784c33d080bc6cf9beffe1ee4e28ee
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2025-11-20 21:33:45 +0700

    improved shit

--- src/components/robot/Gallery.tsx
+++ src/components/robot/Gallery.tsx
@@ -1,5 +1,4 @@
 import useLocale from "@/hooks/useLocale";
-import useInView from "@/hooks/useInView";
 import Text from "@/components/Text";
 import assetMap from "@/data/assetMap";
 import translations from "@/data/translations";
@@ -10,18 +9,30 @@ function Gallery() {
   const t = translations(locale);
   const images = Images[locale];
 
+  const rowSpans = [2, 1, 2, 2, 1, 2, 1, 1, 2];
+
   return (
     <section className="flex flex-col gap-y-page w-full h-fit p-page bg-bg">
       <div className="flex flex-col gap-y-s-four h-fit">
         <Text type="title">{t.robot.gallery.heading}</Text>
         <div className="flex flex-row flex-wrap gap-s-four self-center justify-self-center">
-          <div className="gap-s-three grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 w-full">
-            {images.map((image) => (
-              <div>
-                <img src={assetMap[image.img]}/>
-                <Text type="pg">{image.desc}</Text>
-              </div>
-            ))}
+          <div className="gap-s-three grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 auto-rows-[200px] w-full">
+            {images.map((image, idx) => {
+              const rowSpan = rowSpans[idx % rowSpans.length];
+              return (
+                <div
+                  key={idx}
+                  className="flex flex-col gap-y-s-four"
+                  style={{ gridRow: `span ${rowSpan}` }}
+                >
+                  <img
+                    src={assetMap[image.img]}
+                    className="h-full object-cover"
+                  />
+                  <Text type="sub" className="h-fit">{image.desc}</Text>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
