a86d394f443e5640dd61387a3b0e3d82bcdf5962
architelos <technopiabcde@gmail.com>
2025-12-13 21:00:27 -0800

    t fixes

---
+++ src/pages/Tammy.tsx
@@ -0,0 +1,24 @@
+function Tammy() {
+    return (
+        <>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+            <marquee><h1>nigger nigger nigger nigger nigger</h1></marquee>
+        </>
+    );
+}
+
+export default Tammy;
