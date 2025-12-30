5576db47329a6dff6f29cd3e61b25ed1876c6010
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2025-12-30 13:37:02 +0700

    image fix

--- src/components/robot/Landing.tsx
+++ src/components/robot/Landing.tsx
@@ -9,7 +9,7 @@ function Landing() {
 
   return (
     <main
-      className="relative flex flex-col justify-center items-center w-screen h-dvh -mx-page"
+      className="relative flex flex-col justify-center items-center w-full h-dvh p-page"
     >
       <img
         src={assetMap["pv.avif"]}
@@ -18,7 +18,7 @@ function Landing() {
       />
       <div className="z-1 absolute w-full h-dvh bg-linear-to-t from-30% from-bg/80 to-100% to-bg" />
 
-      <div className="z-2 flex flex-col justify-end items-start gap-y-s-four w-full h-dvh pl-page pr-page pb-page">
+      <div className="z-2 flex flex-col justify-end items-start gap-y-s-four w-full h-dvh">
         <Text type="title">{t.robot.landing.header_text_1}</Text>
         <Text type="title" className="text-accent!">{t.robot.name}</Text>
       </div>
